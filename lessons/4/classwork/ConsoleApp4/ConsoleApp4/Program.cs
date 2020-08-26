ebb8e35e61c92154ca51ae18388e6969fdad3277
Nastasia <nastasia@ndl.ru>
2020-08-26 20:22:04 +0300

    classwork 4 bugfixes

--- lessons/4/classwork/ConsoleApp4/ConsoleApp4/Program.cs
+++ lessons/4/classwork/ConsoleApp4/ConsoleApp4/Program.cs
@@ -18,12 +18,15 @@ namespace ConsoleApp4
                 double doubleH = double.Parse(h);
                 var result1 = 3 * doubleA * doubleH;
                 Console.WriteLine("S1 = " + result1);
-                var result2 = (Math.Sqrt(3)*doubleA + 2*doubleH)*(3/2)*doubleA;
+                var result2 = (Math.Sqrt(3) * doubleA + 2 * doubleH) * (3.0d / 2.0d) * doubleA;
                 Console.WriteLine("S2 = " + result2);
-                var result3 = Math.Sqrt(Math.Pow(doubleH,2) - (Math.Pow(doubleA,2)/12));
+                var result3 = Math.Sqrt(Math.Pow(doubleH, 2) - (Math.Pow(doubleA, 2) / 12.0d));
                 Console.WriteLine("H = " + Math.Round(result3));
-                var result4 = Math.Pow(doubleA,2)/2*result3*Math.Sqrt(doubleH);
+                var result4 = Math.Pow(doubleA, 2) / 2.0d * result3 * Math.Sqrt(doubleH);
                 Console.WriteLine("V = " + result4);
+            }
+            else { 
+            
             }
 
 
