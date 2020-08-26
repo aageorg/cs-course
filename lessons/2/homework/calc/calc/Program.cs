a7c99705ece9554442939caa067c059f4882e64f
Nastasia <nastasia@ndl.ru>
2020-08-26 13:56:12 +0300

    validation bugfix

--- lessons/2/homework/calc/calc/Program.cs
+++ lessons/2/homework/calc/calc/Program.cs
@@ -16,16 +16,19 @@ namespace calc
             a = Console.ReadLine();
             Console.WriteLine("Please, enter the second number:");
             b = Console.ReadLine();
-            if (Regex.IsMatch(a, "^(-)?[0-9]*[.]?[0-9]+$") && Regex.IsMatch(b, "^(-)?[0-9]*[.]?[0-9]+$"))
+            if (Regex.IsMatch(a, "^(-)?[0-9]*[,]?[0-9]+$") && Regex.IsMatch(b, "^(-)?[0-9]*[,]?[0-9]+$"))
             {
-                Console.WriteLine("Please select an operand (1-6):");
+                double doubleA = double.Parse(a);
+                double doubleB = double.Parse(b);
+                Console.WriteLine("Please select an operator (1-6):");
                 Console.WriteLine("1 (+)           3 (*)          5(%)");
                 Console.WriteLine("2 (-)           4 (/)          6(^)\n");
                 string act = Console.ReadLine();
-
+               
                 if (Regex.IsMatch(act, "^[1-6]{1}$"))
                 {
-                    calc.Program.Calculate(double.Parse(a), double.Parse(b), byte.Parse(act));
+                    byte byteAct = byte.Parse(act);
+                    calc.Program.Calculate(doubleA, doubleB, byteAct);
                 }
                 else
                 {
@@ -51,7 +54,6 @@ namespace calc
                 case 1:
                     result = a + b;
                     Console.WriteLine(a + "+ " + b + " = " + result);
-                    
                     break;
                 case 2:
                     result = a - b;
