0db88883ff9a9512e2ee6217e06dc7ebdfc367f6
Nastasia <nastasia@ndl.ru>
2020-08-28 12:06:03 +0300

    Lesson 3 classwork&homework

---
+++ lessons/3/classwork/Objects/Objects/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object a = 5;
+            Console.WriteLine((int)a+5);
+
+            string[] array = new string[5];
+            for (int i = 0; i < array.Length; i++) {
+                Console.Write("Введите " + i + "-й элемент: ");
+                array[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i<array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ lessons/3/homework/Lesson3/Lesson3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Associative array:
+            Console.WriteLine("Ener names and ages of 3 people:");
+            Dictionary<string, int> people = new Dictionary<string, int>();
+            for (int i = 0; i<3; i++)
+            {
+                Console.Write("The name: ");
+                string name = Console.ReadLine();
+                people.Add(name, 0);
+                Console.Write(name + "'s age: ");
+                people[name] = int.Parse(Console.ReadLine()) + 4;
+            }
+            Console.WriteLine("/n");
+            foreach (KeyValuePair<string, int> keyValue in people)
+            {
+                Console.WriteLine(keyValue.Key + " " + keyValue.Value + " y.o.");
+            }
+        }
+    }
+}
