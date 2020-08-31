09ac4b687992409d5c2019d7b33e72cf3c1026ba
Nastasia <nastasia@ndl.ru>
2020-08-31 14:59:46 +0300

    Lesson4 Homework (Enum)

---
+++ lessons/4/homework/Lesson4/Lesson4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+enum Containers
+{
+    Little = 1,
+    Medium = 5,
+    Large = 20   
+    
+}
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int search;
+            Console.WriteLine("Введите количество сока в литрах:");
+            decimal liters = Math.Ceiling(decimal.Parse(Console.ReadLine()));
+            search = (int)liters;
+            Console.WriteLine("You need:");
+
+            for (int i = search; i>0; i--)
+            {
+                if(Enum.IsDefined(typeof(Containers), i) == true)
+                {
+                    Console.WriteLine(search/i + " " + (Containers)i + " container(s) (" + i + "L)");
+                    i = ((int)liters % i);
+                    search = i;
+                }
+            }
+        }
+    }
+}
