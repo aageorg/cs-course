c5a2086184099d2c2bc60ab8095c8cfb6e32cda0
Nastasia <nastasia@ndl.ru>
2020-08-31 10:21:04 +0300

    Lesson2: Removed recursive call Main(), do-while cycle intsead

--- lessons/2/homework/calc/calc/Program.cs
+++ lessons/2/homework/calc/calc/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Linq.Expressions;
-using System.Text.RegularExpressions;
 
 namespace calc
 {
@@ -8,42 +6,32 @@ namespace calc
     {
         static void Main()
         {
-
             string a;
             string b;
+            string act;
+            double doubleA;
+            double doubleB;
+            byte byteAct;
 
-            Console.WriteLine("Please, enter the first number:");
-            a = Console.ReadLine();
-            Console.WriteLine("Please, enter the second number:");
-            b = Console.ReadLine();
-            if (Regex.IsMatch(a, "^(-)?[0-9]*[,]?[0-9]+$") && Regex.IsMatch(b, "^(-)?[0-9]*[,]?[0-9]+$"))
+            do
             {
-                double doubleA = double.Parse(a);
-                double doubleB = double.Parse(b);
+                Console.WriteLine("Please, enter the first number:");
+                a = Console.ReadLine();
+                Console.WriteLine("Please, enter the second number:");
+                b = Console.ReadLine();
+            }
+            while (!double.TryParse(a, out doubleA) || !double.TryParse(b, out doubleB));
+
+            do
+            {
                 Console.WriteLine("Please select an operator (1-6):");
                 Console.WriteLine("1 (+)           3 (*)          5 (%)");
                 Console.WriteLine("2 (-)           4 (/)          6 (^)\n");
-                string act = Console.ReadLine();
-               
-                if (Regex.IsMatch(act, "^[1-6]{1}$"))
-                {
-                    byte byteAct = byte.Parse(act);
-                    calc.Program.Calculate(doubleA, doubleB, byteAct);
-                }
-                else
-                {
-                    Console.WriteLine("Wrong input. ");
-                    calc.Program.Main();
-                }
-
-
-             }
-            else {
-                Console.WriteLine("Wrong input. ");
-                calc.Program.Main();
+                act = Console.ReadLine();
             }
-            
-
+            while (!byte.TryParse(act, out byteAct));
+         
+            calc.Program.Calculate(doubleA, doubleB, byteAct);
         }
   
         static void Calculate(double a, double b, byte act) {
@@ -73,19 +61,8 @@ namespace calc
                     break;
                 case 6:
                     result = Math.Pow(a, b);
-                    Console.WriteLine(a+" ^ "+b+" = "+ result);
+                    Console.WriteLine(a + " ^ " + b + " = " + result);
                     break;
-            }
-            Console.WriteLine("Do you want to calculate something else? (y/n)");
-            string thenext = Console.ReadLine();
-            if (thenext == "y")
-            {
-                calc.Program.Main();
-            }
-            else 
-            {
-                Console.WriteLine("Thank you. Press any key to exit.");
-                Console.ReadLine();
             }
         }
     }
