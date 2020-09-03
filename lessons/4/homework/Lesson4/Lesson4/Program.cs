524c112f76671f6b7bf6ddb87beee5ab587ccec3
Nastasia <nastasia@ndl.ru>
2020-09-03 17:30:26 +0300

    Оптимизирован вход в цикл, добавлены проверкии ввода

--- lessons/4/homework/Lesson4/Lesson4/Program.cs
+++ lessons/4/homework/Lesson4/Lesson4/Program.cs
@@ -7,28 +7,62 @@ enum Containers
 {
     Little = 1,
     Medium = 5,
-    Large = 20   
-    
+    Large = 20,    
 }
 
 namespace Lesson4
 {
     class Program
     {
+        static int MaxContainer()
+        {
+            int max = 0;
+            foreach (int i in Enum.GetValues(typeof(Containers)))
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+            return max;
+        }
+
+        static dynamic ValidateInput(string input)
+        {
+            if (!decimal.TryParse(input.Replace(".", ","), out decimal i))
+            {
+                Console.WriteLine("Недопустимое значение. Введите число.");
+                return false;
+            }
+            if (i > Int32.MaxValue || i < 1)
+            {
+                Console.WriteLine("Допустимый диапазон значений от 1 до "+ Int32.MaxValue + ". Введите число.");
+                return false;
+            }
+            return true;
+        }
+
         static void Main(string[] args)
         {
-            int search;
-            Console.WriteLine("Введите количество сока в литрах:");
-            decimal liters = Math.Ceiling(decimal.Parse(Console.ReadLine()));
-            search = (int)liters;
+            decimal search;
+            string liters;
+            do
+            {
+                Console.WriteLine("Введите количество сока в литрах:");
+                liters = Console.ReadLine();
+            }
+            while (!ValidateInput(liters));
+            
+            search = Math.Ceiling(decimal.Parse(liters.Replace(".",",")));
             Console.WriteLine("You need:");
 
-            for (int i = search; i>0; i--)
+
+            for (int i = MaxContainer(); i>0; i--)
             {
                 if(Enum.IsDefined(typeof(Containers), i) == true)
                 {
-                    Console.WriteLine(search/i + " " + (Containers)i + " container(s) (" + i + "L)");
-                    i = ((int)liters % i);
+                    Console.WriteLine((int)search/i + " " + (Containers)i + " container(s) (" + i + "L)");
+                    i = ((int)search % i);
                     search = i;
                     i++;
                 }
