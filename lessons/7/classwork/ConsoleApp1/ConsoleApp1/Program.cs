693291d0df26c66571859e95250ba7282cfcb477
Nastasia <nastasia@ndl.ru>
2020-09-07 19:56:05 +0300

    strings formatting

---
+++ lessons/7/classwork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers");
+            decimal a = decimal.Parse(Console.ReadLine());
+            decimal b = decimal.Parse(Console.ReadLine());
+            decimal result1 = a + b;
+            decimal result2 = a * b;
+            decimal result3 = a / b;
+            Console.WriteLine(a + " + " + b + " = " + result1.ToString("N2"));
+            Console.WriteLine(String.Format("{0} * {1} = {2:0.##}", a, b, result2));
+            Console.WriteLine($"{a} / {b} = {result3:0.##}");
+        }
+    }
+}
