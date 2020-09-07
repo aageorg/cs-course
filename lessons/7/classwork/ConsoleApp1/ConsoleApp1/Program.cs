b64c71cbb49c740f156d730e686cd923963c6ef1
Nastasia <nastasia@ndl.ru>
2020-09-07 20:59:25 +0300

    strings modifying

--- lessons/7/classwork/ConsoleApp1/ConsoleApp1/Program.cs
+++ lessons/7/classwork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Enter two numbers");
             decimal a = decimal.Parse(Console.ReadLine());
             decimal b = decimal.Parse(Console.ReadLine());
@@ -15,6 +16,21 @@ namespace ConsoleApp1
             Console.WriteLine(a + " + " + b + " = " + result1.ToString("N2"));
             Console.WriteLine(String.Format("{0} * {1} = {2:0.##}", a, b, result2));
             Console.WriteLine($"{a} / {b} = {result3:0.##}");
+            */
+
+            string result1 = "";
+            string result2 = "";
+
+            string text = "    lorem    ipsum     dolor   sit     amet    ";
+            var array = text.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            array[1] = array[1].ToUpper();
+            result1 = string.Join(" ", array);
+            Console.WriteLine(result1);
+            result2 = text.Replace("amet", "").TrimEnd();
+            Console.WriteLine(result2);
+
+            
+
         }
     }
 }
