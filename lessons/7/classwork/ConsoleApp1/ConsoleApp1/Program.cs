557e04a328a400c97a6468263e6db7f4e7f2f25e
Nastasia <nastasia@ndl.ru>
2020-09-09 00:24:24 +0300

    using stringbuilder

--- lessons/7/classwork/ConsoleApp1/ConsoleApp1/Program.cs
+++ lessons/7/classwork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -22,6 +23,7 @@ namespace ConsoleApp1
             string result2 = "";
 
             string text = "    lorem    ipsum     dolor   sit     amet    ";
+            /*
             var array = text.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);
             array[1] = array[1].ToUpper();
             result1 = string.Join(" ", array);
@@ -29,6 +31,88 @@ namespace ConsoleApp1
             int end = text.TrimEnd().LastIndexOf(" ");
             result2 = text.TrimEnd().Substring(0,end);
             Console.WriteLine(result2);
+            */
+            StringBuilder firstSB = new StringBuilder(text, text.Length);
+            bool notFirstWhiteSpace = false;
+
+
+            /******************
+             * The first task
+             ******************/
+
+
+            Console.WriteLine("The first task:");
+            // removes start whitespaces:
+            while (Char.IsWhiteSpace(firstSB[0]))
+            {
+                firstSB.Remove(0, 1);
+            }
+            // removes end whitespaces:
+            while (Char.IsWhiteSpace(firstSB[firstSB.Length - 1]))
+            {
+                firstSB.Remove(firstSB.Length - 1, 1);
+            }
+            //removes duplicate whitespaces in a middle:
+            for (int i = firstSB.Length-1; i >0; i--)               
+            {                
+                if (Char.IsLetterOrDigit(firstSB[i]) || Char.IsPunctuation(firstSB[i]))
+                { 
+                    notFirstWhiteSpace = false;
+                    continue;
+                }
+                if (Char.IsWhiteSpace(firstSB[i]) && notFirstWhiteSpace)
+                {
+                   firstSB.Remove(i, 1);
+                }
+                else
+                {
+                    notFirstWhiteSpace = true;
+                }
+            }
+            // Uppercase second word:
+            for (int i = 0; i < firstSB.Length; i++)
+            {
+                if (Char.IsWhiteSpace(firstSB[i]))
+                {
+                    i++;
+                    while(!Char.IsWhiteSpace(firstSB[i]))
+                    {
+                        firstSB.Replace(firstSB[i], char.ToUpper(firstSB[i]), i,1);
+                        i++;
+                    }
+                    break;
+                }
+            }            
+            Console.WriteLine(firstSB.ToString());
+
+
+            /*****************
+             * The second task
+             *****************/
+
+            Console.WriteLine("\nThe second task:");
+            StringBuilder secondSB = new StringBuilder(text, text.Length*2);
+
+            // Removes last word:
+            for (int i = secondSB.Length-1; i >=0; i--)
+            {
+                if (Char.IsLetterOrDigit(secondSB[i]) || Char.IsPunctuation(secondSB[i]))
+                {
+                    while (!Char.IsWhiteSpace(secondSB[i]))
+                    {
+                        secondSB.Remove(i, 1);
+                        i--;
+                    }
+                    break;
+                }
+            }
+            // removes end whitespaces:
+            while (Char.IsWhiteSpace(secondSB[secondSB.Length - 1]))
+            {
+                secondSB.Remove(secondSB.Length - 1, 1);
+            }
+            Console.WriteLine(secondSB.ToString());
+
         }
     }
 }
