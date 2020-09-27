5c4226ff84843da0e71b6ad192f82dc15cafa4ef
Nastasia <nastasia@ndl.ru>
2020-09-27 12:59:55 +0300

    Lesson 11 homework

---
+++ lessons/11/homework/Alarm/Alarm/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Diagnostics.Contracts;
+
+namespace Alarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReminderItem alarm1 = new ReminderItem("31.12.2020 23:20", "RUN!!!");
+            ReminderItem alarm2 = new ReminderItem("20.09.2020 10:00", "Do something");
+            ReminderItem alarm3 = new ReminderItem("10.10.2020 17:20");
+
+            alarm1.WriteProperties();
+            alarm2.WriteProperties();
+            alarm3.WriteProperties();
+
+        /* 
+        Output:
+
+        TimeToAlarm: 95.10:25:17.0217980
+        IsOutdated: False
+        AlarmDate: 31.12.2020 23:20:00 + 03:00
+        AlarmMessage: RUN!!!
+
+
+        TimeToAlarm: -7.02:54:43.1512119
+        IsOutdated: True
+        AlarmDate: 20.09.2020 10:00:00 + 03:00
+        AlarmMessage: Do something
+
+
+        TimeToAlarm: 13.04:25:16.8467880
+        IsOutdated: False
+        AlarmDate: 10.10.2020 17:20:00 + 03:00
+        AlarmMessage: No message for this alarm
+        */
+        }
+    }
+}
---
+++ lessons/11/homework/Alarm/Alarm/ReminderItem.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Reflection;
+
+namespace Alarm
+{
+    class ReminderItem
+    {
+        private DateTimeOffset _alarmDate;
+        private string _alarmMessage;
+
+        public TimeSpan TimeToAlarm
+        {
+            get => AlarmDate - DateTimeOffset.Now;
+        }
+        public bool IsOutdated
+        {
+            get
+            {
+                if (TimeToAlarm.Seconds > 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public DateTimeOffset AlarmDate
+        {
+            get => _alarmDate;
+            set
+            {
+                _alarmDate = value;
+            }               
+            
+        }
+
+        public string AlarmMessage
+        {
+            get => _alarmMessage;
+            set
+            {
+                _alarmMessage = value;
+            }
+        }
+
+
+        public void WriteProperties()
+        {
+            foreach (PropertyInfo prop in typeof(ReminderItem).GetProperties())
+            {
+                Console.WriteLine($"{prop.Name}: {prop.GetValue(this)}");
+            }
+            Console.WriteLine("\n");
+        }
+        public ReminderItem(string datetime, string message  = "")
+        {
+            if (!DateTimeOffset.TryParse(datetime, out DateTimeOffset alarmdate)) {
+                throw new ArgumentException ("Cannot parse the date of alarm value");
+            }
+            AlarmDate = alarmdate;
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                AlarmMessage = "No message for this alarm";
+            }
+
+        else
+            {
+                AlarmMessage = message;
+            }
+               
+        }
+
+    }
+}
